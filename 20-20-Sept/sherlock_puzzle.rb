0ba34b3e49d8cfbea9453083fe9f5028497f411e
hihell <jiu694@gmail.com>
2013-09-19 23:22:44 +0800

    optimized, removed sort
    add energy map remake test, removed unnecessary sort

--- 20-20-Sept/sherlock_puzzle.rb
+++ 20-20-Sept/sherlock_puzzle.rb
@@ -1,29 +1,27 @@
+require 'benchmark'
 
-def cal_short(string, energy)
+def cal_short(string, energy, energy_map)
 #	puts string
 	h = Hash.new
-	a = Array.new
+	a = energy_map
 	mid = -1
 	ce = 0
-	string.chars.each_with_index do |e, i|
-	
-		if(e == '0')
-			ce -= 2
-		elsif(e == '1')
-			ce += 3
-		elsif(e == '2')
-			ce += energy
-			mid = i
-		end 	
+	if(!energy_map) # if we don't have energy map, make it!
+		a = Array.new
+		string.chars.each_with_index do |e, i|	
+			if(e == '0')
+				ce -= 2
+			elsif(e == '1')
+				ce += 3
+			elsif(e == '2')
+				ce += energy
+				mid = i
+			end 	
 
-		if(!h[ce])
-			h[ce] = Array.new
+			h[ce] = i
+			a[i] = ce
 		end
-		h[ce].push(i)
-		a[i] = ce
 	end
-	# puts a
-	# puts h
 
 	max_len = 0
 	string.chars.each_with_index do |e, i|
@@ -36,16 +34,15 @@ def cal_short(string, energy)
 		elsif(e == '2')
 			lv = a[i] - energy
 		end
-		
-		max_0 = h[lv] != nil ? h[lv].max : i
-		max_1 = h[lv + 1] != nil ? h[lv + 1].max : i
-		
+		max_0 = h[lv] != nil ? h[lv] : i
+		max_1 = h[lv + 1] != nil ? h[lv + 1] : i
+
 		max01 = [max_0, max_1].max
 		max01 = a.last >= lv ? a.length - 1 : max01	
 		if(max01 >= mid)
 
 			max_len = max_len > max01 - i + 1 ? max_len : max01 - i + 1
-			 #puts "max01: #{max01}, i: #{i}, mid: #{mid}"
+			
 			if(string[max01] == '0' and max_len == 1)
 				max_len = 0
 			end
@@ -55,15 +52,15 @@ def cal_short(string, energy)
 			break
 		end
 	end
-	# puts "rest len:#{max_len}"
+	
 	return max_len
 end
 
 
 def cal()
+	
 	k, s = gets.chomp.split(' ')
-	k = k.to_i
-	#puts "length: #{s.length}"
+	k = k.to_i	
 	
 	if(k > 2)
 		crt = 0
@@ -83,7 +80,6 @@ def cal()
 
 		lm = la.each_with_index.max
 		rm = ra.each_with_index.max
-		 #puts "lm:#{lm}, rm:#{rm}"
 		
 		totalE = 0
 		totalL = 0
@@ -100,30 +96,28 @@ def cal()
 			remainL = s.slice(0, s.length - rm[1] - 1)
 		end
 
-		#puts "e:#{e}, totalE:#{totalE}"
 		if(e >= 0)
 			return k * s.length
 		elsif(totalE + e >= 0)
 			num = (totalE / e.abs) < (k - 2) ? (totalE / e.abs) : (k - 2)
-			 #puts "num: #{num}, totalE: #{totalE}, totalL: #{totalL}"
+			 
 			len = num * s.length + totalL
-			# puts "len: #{len}"
+			
 			remain = totalE + num * e
-			# puts "remain: #{remain}"
+			
 			if(remain > 0)		
 				# use the remaining energy to make it longer
 				rest = remainL + '2' + remainR
-				#puts "rest: #{rest}, remain: #{remain}"
 				# the short array passed to function contains 1 pseudo char
 				# remove that from the length 
-				len += cal_short(rest, remain) - 1
+				len += cal_short(rest, remain, nil) - 1
 			end
 			return len 
 		else
 			if(k >= 2)
-				return cal_short(s+s, 0)
+				return cal_short(s+s, 0, nil)
 			else
-				return cal_short(s, 0)
+				return cal_short(s, 0, la)
 			end
 		end
 
@@ -133,13 +127,17 @@ def cal()
 			ss += s
 		end
 		
-		return cal_short(ss, 0)
+		return cal_short(ss, 0, nil)
 	end
 
 end
 
 def main()
-	puts cal()
+	# Benchmark.bm do |b|
+		# b.report {
+			puts cal()
+		# }
+	# end
 end
 
 main()
